e8a2ff620cc208ed21d0d061f4b60a4305ce3f8e
mluchkin <maksym.luchkin@gmail.com>
2019-10-10 12:29:43 +0300

    added new assert examples

---
+++ assert_examples.py
@@ -0,0 +1,9 @@
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, \
+        f"expected {expected_result}, got {actual_result}"
+
+def test_substring(full_string, substring):
+    assert "substring" in "full_string", \
+        f"expected '{substring}' to be substring of '{full_string}'"
+    # ваша реализация, напишите assert и сообщение об ошибке
+
